0feec3078fc57d4c4dab0767e2a32d24fa9b2383
AndreLLLeoncio <leoncioandreluislopes@gmail.com>
2026-05-12 14:31:04 -0400

    salvando

--- compilador.py
+++ compilador.py
@@ -3,9 +3,9 @@ import logging
 from erro import Erro
 from analise_lexica import AnaliseLexica
 from analise_sintatica import AnaliseSintatica
-from analise_semantica import AnaliseSemantica
-from geracao_tac import GeracaoTAC
-from geracao_llvm import GeracaoLLVM
+# from analise_semantica import AnaliseSemantica
+# from geracao_tac import GeracaoTAC
+# from geracao_llvm import GeracaoLLVM
 
 class Compilador:
     def __init__(self, nome_arquivo_fonte: str):
@@ -16,11 +16,11 @@ class Compilador:
         # Instanciar as fases, passando o manipulador de erros
         self.analise_lexica_mod = AnaliseLexica(self.erro_handler)
         self.analise_sintatica_mod = AnaliseSintatica(self.erro_handler)
-        self.analise_semantica_mod = AnaliseSemantica(self.erro_handler)
-        self.geracao_tac_mod = GeracaoTAC(self.erro_handler)
-        self.geracao_llvm_mod = GeracaoLLVM(self.erro_handler)
+        # self.analise_semantica_mod = AnaliseSemantica(self.erro_handler)
+        # self.geracao_tac_mod = GeracaoTAC(self.erro_handler)
+        # self.geracao_llvm_mod = GeracaoLLVM(self.erro_handler)
 
-        self.logger.info(f"Compilador BASIQuinho inicializado para o arquivo: {nome_arquivo_fonte}")
+        self.logger.info(f"Compilador Portugoool inicializado para o arquivo: {nome_arquivo_fonte}")
 
     def compilar(self):
         self.logger.info(f"--- Iniciando compilação de {self.nome_arquivo_fonte} ---")
@@ -44,32 +44,32 @@ class Compilador:
         self.analise_sintatica_mod.exportarAST_SVG(base_nome_arquivo + ".dot", base_nome_arquivo + ".svg")
 
         # Fase 3: Análise Semântica
-        self.logger.info("--- FASE: Análise Semântica ---")
-        ast_anotada = self.analise_semantica_mod.executarAnaliseSemantica(ast)
-        if self.erro_handler.tem_erros_semanticos or not ast_anotada:
-            self.logger.error("Compilação interrompida devido a erros semânticos.")
-            return False
+        # self.logger.info("--- FASE: Análise Semântica ---")
+        # ast_anotada = self.analise_semantica_mod.executarAnaliseSemantica(ast)
+        # if self.erro_handler.tem_erros_semanticos or not ast_anotada:
+        #     self.logger.error("Compilação interrompida devido a erros semânticos.")
+        #     return False
 
         # Fase 4: Geração de Código de Três Endereços (TAC)
-        self.logger.info("--- FASE: Geração de Código de Três Endereços (TAC) ---")
-        codigo_tac = self.geracao_tac_mod.gerarCodigoTAC(ast_anotada)
-        if self.erro_handler.tem_erros_tac or not codigo_tac:
-            self.logger.error("Compilação interrompida devido a erros na geração do TAC.")
-            return False
+        # self.logger.info("--- FASE: Geração de Código de Três Endereços (TAC) ---")
+        # codigo_tac = self.geracao_tac_mod.gerarCodigoTAC(ast_anotada)
+        # if self.erro_handler.tem_erros_tac or not codigo_tac:
+        #     self.logger.error("Compilação interrompida devido a erros na geração do TAC.")
+        #     return False
 
-        # Fase 5: Geração de Código LLVM
-        self.logger.info("--- FASE: Geração de Código LLVM IR ---")
-        codigo_llvm = self.geracao_llvm_mod.gerarCodigoLLVM(codigo_tac)
-        if self.erro_handler.tem_erros_llvm or not codigo_llvm:
-            self.logger.error("Compilação interrompida devido a erros na geração do LLVM IR.")
-            return False
+        # # Fase 5: Geração de Código LLVM
+        # self.logger.info("--- FASE: Geração de Código LLVM IR ---")
+        # codigo_llvm = self.geracao_llvm_mod.gerarCodigoLLVM(codigo_tac)
+        # if self.erro_handler.tem_erros_llvm or not codigo_llvm:
+        #     self.logger.error("Compilação interrompida devido a erros na geração do LLVM IR.")
+        #     return False
 
-        self.logger.info("--- Compilação concluída com sucesso! ---")
-        try:
-            with open(base_nome_arquivo + ".ll", "w", encoding='utf-8') as f:
-                f.write(codigo_llvm)
-            self.logger.info(f"Código LLVM IR salvo em: {base_nome_arquivo}.ll")
-        except IOError as e:
-            self.erro_handler.registrar_erro("Compilador",0,0,f"Erro ao salvar arquivo LLVM: {e}", "GERAL")
-            return False
+        # self.logger.info("--- Compilação concluída com sucesso! ---")
+        # try:
+        #     with open(base_nome_arquivo + ".ll", "w", encoding='utf-8') as f:
+        #         f.write(codigo_llvm)
+        #     self.logger.info(f"Código LLVM IR salvo em: {base_nome_arquivo}.ll")
+        # except IOError as e:
+        #     self.erro_handler.registrar_erro("Compilador",0,0,f"Erro ao salvar arquivo LLVM: {e}", "GERAL")
+        #     return False
         return True
